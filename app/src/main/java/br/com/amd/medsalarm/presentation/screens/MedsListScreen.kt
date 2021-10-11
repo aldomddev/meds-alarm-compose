5788680b6e27cebe8fe35f54dbda5b8429f04656
aldomddev <aldomddev@gmail.com>
2021-10-11 18:35:47 -0300

    empty list state - minor change

--- app/src/main/java/br/com/amd/medsalarm/presentation/screens/MedsListScreen.kt
+++ app/src/main/java/br/com/amd/medsalarm/presentation/screens/MedsListScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
@@ -166,10 +167,16 @@ private fun ItemDropdownMenu(
 
 @Composable
 private fun EmptyList() {
-    Text(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        text = stringResource(id = R.string.alarm_list_no_medications)
-    )
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            textAlign = TextAlign.Center,
+            text = stringResource(id = R.string.alarm_list_no_medications)
+        )
+    }
 }
 
 private fun MedsAlarmVO.toNextAlarmString(): String {
