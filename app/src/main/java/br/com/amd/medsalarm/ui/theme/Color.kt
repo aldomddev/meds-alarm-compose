6ce6c533e481eb9f30a11297b70e410843ccc52a
aldomddev <aldomddev@gmail.com>
2021-10-21 20:38:25 -0300

    added new colors to palette

--- app/src/main/java/br/com/amd/medsalarm/ui/theme/Color.kt
+++ app/src/main/java/br/com/amd/medsalarm/ui/theme/Color.kt
@@ -6,3 +6,11 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// light
+val LightBlue800 = Color(0xFF0277BD)
+val Green500 = Color(0xFF4CAF50)
+
+// dark
+val LightBlue200 = Color(0xFF81D4FA)
+val Green200 = Color(0xFFA5D6A7)
