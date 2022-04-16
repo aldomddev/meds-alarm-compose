33faa97eb3d8fc8e707f0f02173ac615907c9edb
aldomddev <aldomddev@gmail.com>
2022-04-16 13:26:53 -0300

    Versions update

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,31 +1,31 @@
 object Versions {
-    const val compose = "1.1.0-alpha05"
+    const val compose = "1.1.1"
 
-    const val androidMaterial = "1.4.0"
+    const val androidMaterial = "1.5.0"
 
-    const val androidxActivityCompose = "1.3.1"
-    const val androidxAppCompat = "1.3.1"
+    const val androidxActivityCompose = "1.4.0"
+    const val androidxAppCompat = "1.4.1"
     const val androidxComposeMaterial = compose
     const val androidxComposeRuntimeLiveData = "1.0.2"
     const val androidxComposeUi = compose
     const val androidxComposeUiTooling = compose
     const val androidxComposeUiToolingPreview = compose
-    const val androidxCore = "1.6.0"
-    const val androidxHiltNavigationCompose = "1.0.0-alpha03"
-    const val androidxLifecycle = "2.4.0-alpha03"
-    const val androidxNavigationCompose = "2.4.0-alpha10"
-    const val androidxRoom = "2.3.0"
+    const val androidxCore = "1.7.0"
+    const val androidxHiltNavigationCompose = "1.0.0"
+    const val androidxLifecycle = "2.4.1"
+    const val androidxNavigationCompose = "2.4.2"
+    const val androidxRoom = "2.4.2"
 
-    const val materialDialogsDateTime = "0.6.1"
+    const val materialDialogsDateTime = "0.7.0"
 
-    const val gradleVersionsPlugin = "0.39.0"
-    const val hiltAndroid = "2.39.1"
+    const val gradleVersionsPlugin = "0.42.0"
+    const val hiltAndroid = "2.41"
     const val hiltExtensionCompiler = "1.0.0"
-    const val jetBrainsCoroutines = "1.5.2"
+    const val jetBrainsCoroutines = "1.6.1"
 
     const val timber = "5.0.1"
 
-    const val workManager = "2.7.0-rc01"
+    const val workManager = "2.7.1"
 
     // tests
     const val androidxComposeJunit = compose
