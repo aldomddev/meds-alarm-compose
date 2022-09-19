a86b6f9b99eb3e4f5c53285c3a7dc6d8e4a4f727
aldomddev <aldomddev@gmail.com>
2022-09-19 19:44:18 -0300

    Versions updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     namespace = "br.com.amd.medsalarm"
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "br.com.amd.medsalarm"
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,30 +1,30 @@
 object Versions {
-    const val compose = "1.2.0"
+    private const val compose = "1.2.1"
 
-    const val accompanist = "0.23.1"
+    const val accompanist = "0.25.1"
 
     const val androidMaterial = "1.6.1"
 
-    const val androidxActivityCompose = "1.5.0"
-    const val androidxAppCompat = "1.4.2"
+    const val androidxActivityCompose = "1.5.1"
+    const val androidxAppCompat = "1.5.1"
     const val androidxComposeMaterial = compose
     const val androidxComposeRuntimeLiveData = "1.0.2"
     const val androidxComposeUi = compose
     const val androidxComposeUiTooling = compose
     const val androidxComposeUiToolingPreview = compose
-    const val androidxComposeCompiler = "1.2.0"
-    const val androidxCore = "1.8.0"
+    const val androidxComposeCompiler = "1.3.1"
+    const val androidxCore = "1.9.0"
     const val androidxHiltNavigationCompose = "1.0.0"
-    const val androidxLifecycle = "2.5.0"
-    const val androidxNavigationCompose = "2.5.0"
-    const val androidxRoom = "2.4.2"
+    const val androidxLifecycle = "2.5.1"
+    const val androidxNavigationCompose = "2.5.2"
+    const val androidxRoom = "2.4.3"
 
-    const val desugarJdkLibs = "1.1.6"
+    const val desugarJdkLibs = "1.2.2"
 
     const val materialDialogsDateTime = "0.8.1-rc"
 
     const val gradleVersionsPlugin = "0.42.0"
-    const val hiltAndroid = "2.42"
+    const val hiltAndroid = "2.43.2"
     const val hiltExtensionCompiler = "1.0.0"
     const val jetBrainsCoroutines = "1.6.4"
 
