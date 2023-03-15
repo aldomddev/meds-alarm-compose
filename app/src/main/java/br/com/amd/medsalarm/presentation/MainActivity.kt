f238b412e6fb2a1ac1a1477c725f9a32822658bb
aldomddev <aldomddev@gmail.com>
2023-03-15 20:59:28 -0300

    fix: notifications for API 33

--- app/src/main/java/br/com/amd/medsalarm/presentation/MainActivity.kt
+++ app/src/main/java/br/com/amd/medsalarm/presentation/MainActivity.kt
@@ -1,14 +1,19 @@
 package br.com.amd.medsalarm.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import br.com.amd.medsalarm.presentation.screens.MainScreen
 import br.com.amd.medsalarm.ui.theme.MedsAlarmTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -17,6 +22,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @ExperimentalMaterialApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -33,6 +40,18 @@ class MainActivity : ComponentActivity() {
 
                 MainScreen("Meds Alarm")
             }
+        }
+
+        requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            println("AMD - Notification permission granted = $it")
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
     }
 }
