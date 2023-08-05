3b1189b0fa46cab26819a3435005153bdc971799
aldomddev <aldomddev@gmail.com>
2023-08-05 13:38:26 -0300

    removing unused extention

--- app/src/main/java/br/com/amd/medsalarm/core/extentions/LiveDataExt.kt
+++
@@ -1,6 +0,0 @@
-package br.com.amd.medsalarm.core.extentions
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-
-fun <T> MutableLiveData<T>.toLiveData(): LiveData<T> = this
